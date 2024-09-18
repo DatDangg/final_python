4f29976965e298f042ca1fc88d25aa15db60ac59
DatDangg <datdang20112003@gmail.com>
2024-09-19 00:41:33 +0700

    check

--- Frontend/python_final/src/components/BestSellingProducts/BestSellingProducts.jsx
+++ Frontend/python_final/src/components/BestSellingProducts/BestSellingProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import './bestsell.css';
 import ProductItem from '../ProductItem/ProductItem'; // Import ProductItem component
@@ -6,96 +6,90 @@ import ProductItem from '../ProductItem/ProductItem'; // Import ProductItem comp
 const BestSellingProducts = () => {
     const [products, setProducts] = useState([]);
     const token = localStorage.getItem("token");
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [itemsPerPage, setItemsPerPage] = useState(6);
+    const containerRef = useRef(null); // Ref for scrollable container
+    const itemWidth = 240; // Chiều rộng của mỗi sản phẩm cộng với margin
+
     // Fetch dữ liệu từ API
     useEffect(() => {
-        axios.get('http://localhost:8000/best-selling-products/',  {
+        axios.get('http://localhost:8000/best-selling-products/', {
             headers: {
               Authorization: `Token ${token}`,
               "Content-Type": "application/json",
             },
           })
-        
           .then(response => {
-            console.log(); // Thêm dòng này để log dữ liệu từ API
+            console.log(response.data); // Log dữ liệu từ API
             setProducts(response.data);
           })
           .catch(error => {
             console.error('Error fetching best selling products:', error);
           });
-      }, []);
-      
+      }, [token]);
     
-  const scrollLeft = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-      categoriesRef.current.style.transform = `translateX(-${
-        (currentIndex - 1) * 190
-      }px)`;
-    }
-  };
+    const scrollLeft = () => {
+        // Cuộn danh sách sang trái
+        if (containerRef.current) {
+            containerRef.current.scrollBy({ left: -itemWidth * 4, behavior: 'smooth' });
+        }
+    };
 
-  const scrollRight = () => {
-    const totalItems = response.data.length.length;
-    if (currentIndex < totalItems - itemsPerPage) {
-      setCurrentIndex(currentIndex + 1);
-      categoriesRef.current.style.transform = `translateX(-${
-        (currentIndex + 1) * 190
-      }px)`;
-    }
-  };
+    const scrollRight = () => {
+        // Cuộn danh sách sang phải
+        if (containerRef.current) {
+            containerRef.current.scrollBy({ left: itemWidth * 4, behavior: 'smooth' });
+        }
+    };
 
-  
     return (
-      <div>
-        <div className="header-container">
-            <h2>Best Selling Products</h2>
-            <div className="scroll-buttons-container">
-          <button className="scroll-button" onClick={scrollLeft}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="24"
-              viewBox="0 0 14 24"
-              fill="none"
-            >
-              <path
-                d="M12.3333 23.6667C12.0679 23.6679 11.8132 23.5622 11.6267 23.3733L0.959995 12.7067C0.570057 12.3162 0.570057 11.6838 0.959995 11.2933L11.6267 0.626666C12.0207 0.259521 12.6347 0.270354 13.0155 0.65117C13.3963 1.03199 13.4071 1.64599 13.04 2.04L3.07999 12L13.04 21.96C13.4299 22.3504 13.4299 22.9829 13.04 23.3733C12.8535 23.5622 12.5987 23.6679 12.3333 23.6667Z"
-                fill="#2E2E2E"
-              />
-            </svg>
-          </button>
-          <button className="scroll-button" onClick={scrollRight}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="24"
-              viewBox="0 0 14 24"
-              fill="none"
-              transform="scale(-1, 1)"
-            >
-              <path
-                d="M12.3333 23.6667C12.0679 23.6679 11.8132 23.5622 11.6267 23.3733L0.959995 12.7067C0.570057 12.3162 0.570057 11.6838 0.959995 11.2933L11.6267 0.626666C12.0207 0.259521 12.6347 0.270354 13.0155 0.65117C13.3963 1.03199 13.4071 1.64599 13.04 2.04L3.07999 12L13.04 21.96C13.4299 22.3504 13.4299 22.9829 13.04 23.3733C12.8535 23.5622 12.5987 23.6679 12.3333 23.6667Z"
-                fill="#2E2E2E"
-              />
-            </svg>
-          </button>
+        <div>
+            <div className="header-container">
+                <h2>Best Selling Products</h2>
+                <div className="scroll-buttons-container">
+                    <button className="scroll-button" onClick={scrollLeft}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="14"
+                            height="24"
+                            viewBox="0 0 14 24"
+                            fill="none"
+                        >
+                            <path
+                                d="M12.3333 23.6667C12.0679 23.6679 11.8132 23.5622 11.6267 23.3733L0.959995 12.7067C0.570057 12.3162 0.570057 11.6838 0.959995 11.2933L11.6267 0.626666C12.0207 0.259521 12.6347 0.270354 13.0155 0.65117C13.3963 1.03199 13.4071 1.64599 13.04 2.04L3.07999 12L13.04 21.96C13.4299 22.3504 13.4299 22.9829 13.04 23.3733C12.8535 23.5622 12.5987 23.6679 12.3333 23.6667Z"
+                                fill="#2E2E2E"
+                            />
+                        </svg>
+                    </button>
+                    <button className="scroll-button" onClick={scrollRight}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="14"
+                            height="24"
+                            viewBox="0 0 14 24"
+                            fill="none"
+                            transform="scale(-1, 1)"
+                        >
+                            <path
+                                d="M12.3333 23.6667C12.0679 23.6679 11.8132 23.5622 11.6267 23.3733L0.959995 12.7067C0.570057 12.3162 0.570057 11.6838 0.959995 11.2933L11.6267 0.626666C12.0207 0.259521 12.6347 0.270354 13.0155 0.65117C13.3963 1.03199 13.4071 1.64599 13.04 2.04L3.07999 12L13.04 21.96C13.4299 22.3504 13.4299 22.9829 13.04 23.3733C12.8535 23.5622 12.5987 23.6679 12.3333 23.6667Z"
+                                fill="#2E2E2E"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div className="best-selling-products-container" ref={containerRef}>
+                <div className="best-selling-products">
+                    {products.length > 0 ? (
+                        products.map((product, index) => (
+                            // Sử dụng ProductItem để hiển thị từng sản phẩm
+                            <ProductItem key={index} product={product} token={localStorage.getItem('token')} />
+                        ))
+                    ) : (
+                        <p>No best selling products found</p>
+                    )}
+                </div>
+            </div>
         </div>
-        </div>
-        <div className="best-selling-products">
-          {products.length > 0 ? (
-            products.map((product, index) => (
-              // Sử dụng ProductItem để hiển thị từng sản phẩm
-              <ProductItem key={index} product={product} token={localStorage.getItem('token')} />
-            ))
-          ) : (
-            <p>No best selling products found</p>
-          )}
-        </div>
-      </div>
     );
-  };
-  
-  export default BestSellingProducts;
-  
+};
+
+export default BestSellingProducts;
