74c115fddfcfc026c1e3c2fe030152468f7f6659
Duong <94699404+ntzang@users.noreply.github.com>
2024-09-21 14:40:33 +0700

    fix

--- Frontend/python_final/src/components/Header/Header.jsx
+++ Frontend/python_final/src/components/Header/Header.jsx
@@ -71,7 +71,7 @@ function Header() {
   }, [searchQuery]);
 
   return (
-    <header className="header">
+    <header className="header container-fluid">
       <img className="logo" alt="Logo" src="../../../logo.jpg" />
       <form className="search-field" onSubmit={handleSearch}>
         <svg
@@ -96,7 +96,6 @@ function Header() {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
       </form>
-
       {/* Suggestions Dropdown */}
       {suggestions.length > 0 && (
         <div className="suggestions-dropdown">
@@ -148,86 +147,100 @@ function Header() {
       <div className="icons">
         <Link to="/wishlist">
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            onClick={handleWishlistClick}
-          >
-            <path
-              d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z"
-              stroke="black"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </Link>
-        <Link to="/cart">
-          <div style={{ position: "relative" }}>
-            <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
               height="32"
               viewBox="0 0 32 32"
               fill="none"
-            >
-              <path
-                d="M3 5H7L10 22H26M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97372 27.8239 6.90679 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66662 27.39 6.66667H8M12 26C12 26.5523 11.5523 27 11 27C10.4477 27 10 26.5523 10 26C10 25.4477 10.4477 25 11 25C11.5523 25 12 25.4477 12 26ZM26 26C26 26.5523 25.5523 27 25 27C24.4477 27 24 26.5523 24 26C24 25.4477 24.4477 25 25 25C25.5523 25 26 25.4477 26 26Z"
+              onClick={handleWishlistClick}
+          >
+            <path
+                d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z"
                 stroke="black"
                 strokeWidth="1.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
+        <Link to="/cart">
+          <div style={{position: "relative"}}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+            >
+              <path
+                  d="M3 5H7L10 22H26M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97372 27.8239 6.90679 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66662 27.39 6.66667H8M12 26C12 26.5523 11.5523 27 11 27C10.4477 27 10 26.5523 10 26C10 25.4477 10.4477 25 11 25C11.5523 25 12 25.4477 12 26ZM26 26C26 26.5523 25.5523 27 25 27C24.4477 27 24 26.5523 24 26C24 25.4477 24.4477 25 25 25C25.5523 25 26 25.4477 26 26Z"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
               />
             </svg>
             {cartItemCount > 0 && (
-              <span className="cart-count">{cartItemCount}</span>
+                <span className="cart-count">{cartItemCount}</span>
             )}
           </div>
         </Link>
-        {token ? (
-          <div className="dropdown">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="user-icon"
-              onClick={toggleDropdown}
-            >
+        <div className="profile-dropdown">
+          <Link to="/profile">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 7.5a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM5.25 18.75A6.75 6.75 0 0 1 12 15a6.75 6.75 0 0 1 6.75 3.75"
-              />
-            </svg>
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <Link to="/profile">Profile</Link>
-                <button onClick={handleLogout}>Logout</button>
-              </div>
-            )}
-          </div>
-        ) : (
-          <Link to="/login">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="user-icon"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6A2.25 2.25 0 0 0 5.25 5.25V9m6 4.5V21m0 0 3-3m-3 3-3-3"
-              />
+                  d="M24 27V24.3333C24 22.9188 23.5224 21.5623 22.6722 20.5621C21.8221 19.5619 20.669 19 19.4667 19H11.5333C10.331 19 9.17795 19.5619 8.32778 20.5621C7.47762 21.5623 7 22.9188 7 24.3333V27M21 9.5C21 11.9853 18.9853 14 16.5 14C14.0147 14 12 11.9853 12 9.5C12 7.01472 14.0147 5 16.5 5C18.9853 5 21 7.01472 21 9.5Z"
+                  stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </Link>
-        )}
+          <div className="dropdown-menu">
+            <Link to="/profile" className="dropdown-item">Tài Khoản Của Tôi</Link>
+            <Link to="/cart" className="dropdown-item">Đơn Mua</Link>
+            <div className="dropdown-item" onClick={handleLogout}>Đăng Xuất</div>
+          </div>
+        </div>
+        {/*{token ? (*/}
+        {/*    <div className="dropdown bg-danger">*/}
+        {/*      <svg*/}
+        {/*          xmlns="http://www.w3.org/2000/svg"*/}
+        {/*          fill="none"*/}
+        {/*          viewBox="0 0 24 24"*/}
+        {/*          strokeWidth="1.5"*/}
+        {/*          stroke="currentColor"*/}
+        {/*          className="user-icon"*/}
+        {/*          onClick={toggleDropdown}*/}
+        {/*      >*/}
+        {/*        <path*/}
+        {/*            strokeLinecap="round"*/}
+        {/*            strokeLinejoin="round"*/}
+        {/*            d="M15.75 7.5a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM5.25 18.75A6.75 6.75 0 0 1 12 15a6.75 6.75 0 0 1 6.75 3.75"*/}
+        {/*        />*/}
+        {/*      </svg>*/}
+        {/*      {isDropdownOpen && (*/}
+        {/*          <div className="dropdown-content">*/}
+        {/*            <Link to="/profile">Profile</Link>*/}
+        {/*            <button onClick={handleLogout}>Logout</button>*/}
+        {/*          </div>*/}
+        {/*      )}*/}
+        {/*    </div>*/}
+        {/*) : (*/}
+        {/*    <Link to="/login">*/}
+        {/*      <svg*/}
+        {/*          xmlns="http://www.w3.org/2000/svg"*/}
+        {/*          fill="none"*/}
+        {/*          viewBox="0 0 24 24"*/}
+        {/*          strokeWidth="1.5"*/}
+        {/*          stroke="currentColor"*/}
+        {/*          className="user-icon"*/}
+        {/*      >*/}
+        {/*        <path*/}
+        {/*            strokeLinecap="round"*/}
+        {/*            strokeLinejoin="round"*/}
+        {/*            d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6A2.25 2.25 0 0 0 5.25 5.25V9m6 4.5V21m0 0 3-3m-3 3-3-3"*/}
+        {/*        />*/}
+        {/*      </svg>*/}
+        {/*    </Link>*/}
+        {/*)}*/}
       </div>
     </header>
   );
